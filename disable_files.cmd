a3fadfb039fc7c0350f2174f2311dec6489c4c06
gigaSecure <105108954+gigaSecure@users.noreply.github.com>
2022-09-15 20:32:00 +0800

    Added Files

---
+++ disable_files.cmd
@@ -0,0 +1,24 @@
+git update-index --assume-unchanged .obsidian/community-plugins.json
+git update-index --assume-unchanged .obsidian/appearance.json
+git update-index --assume-unchanged .obsidian/workspace
+git update-index --assume-unchanged .obsidian/graph.json
+git update-index --assume-unchanged .obsidian/graph.json
+git update-index --assume-unchanged .obsidian/app.json
+git update-index --assume-unchanged .obsidian/core-plugins.json
+git update-index --assume-unchanged .obsidian/hotkeys.json
+
+::Plugins
+git update-index --assume-unchanged .obsidian/plugins/obsidian-minimal-settings/data.json
+git update-index --assume-unchanged .obsidian/plugins/obsidian-minimal-settings/main.js
+
+
+git update-index --assume-unchanged .obsidian/plugins/obsidian-relative-line-numbers/main.js
+git update-index --assume-unchanged .obsidian/plugins/obsidian-relative-line-numbers/manifest.json
+
+
+git update-index --assume-unchanged .obsidian/plugins/obsidian-style-settings/data.json
+git update-index --assume-unchanged .obsidian/plugins/obsidian-style-settings/main.js
+git update-index --assume-unchanged .obsidian/plugins/obsidian-style-settings/manifest.json
+git update-index --assume-unchanged .obsidian/plugins/obsidian-style-settings/styles.css
+
+git update-index --assume-unchanged .obsidian/plugins/obsidian-vimrc-support/data.json
